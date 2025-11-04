d871c778e77e73e052d3938692917f7f7047149e
ShubhSam7 <shubhsamaiya7@gmail.com>
2025-11-04 13:52:58 +0530

    comment POST is working

--- app/api/v1/comments/route.ts
+++ app/api/v1/comments/route.ts
@@ -1,5 +1,75 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/lib/prisma";
+import { middleware } from "@/lib/middleware";
 
-export function POST(req: NextRequest){
-    
+const validateSchema = z.object({
+  content: z.string().min(1).max(100),
+  postId: z.string(),
+});
+
+export async function POST(req: NextRequest) {
+  try {
+    const auth = await middleware(req);
+    const body = await req.json();
+    console.log("middleware checked")
+    if (!auth.success) {
+      return NextResponse.json(
+        { error: auth.error || "Unauthorized" },
+        { status: 401 }
+      );
+    }
+    console.log("validating")
+    const validatedData = validateSchema.parse(body);
+    console.log("validated")
+    if (!validatedData.content) {
+      return NextResponse.json(
+        {
+          msg: "validated schema not verified",
+        },
+        { status: 401 }
+      );
+    }
+    console.log("starting prisma")
+    const user = await prisma.comment.create({
+      //@ts-ignore
+      data: {
+        content: validatedData.content.trim(),
+        authorId: auth.userId,
+        postId: validatedData.postId,
+      },
+      include: {
+        author: {
+          select: {
+            id: true,
+            user_handle: true,
+            avatar: true,
+            bio: true,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json({
+      msg: `Comment is create by ${user.authorId} on ${user.postId} that is `,
+      content: user.content,
+    });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        { error: "Validation failed", details: error.issues },
+        { status: 400 }
+      );
+    }
+
+    console.error("Create post error:", error);
+    return NextResponse.json(
+      { error: "Failed to create post" },
+      { status: 500 }
+    );
+  }
 }
+
+export function GET(req: NextRequest) {}
+
+export function DELETE(req: NextRequest) {}
